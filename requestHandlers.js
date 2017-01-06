f33f236133115466ecb14f67a83a427301235f13
Daniel Vianna <dvianna@bunnings.com.au>
2017-01-06 13:23:22 +0800

    form

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,13 +4,22 @@ const exec = require('child_process').exec
 
 const start = (response) => {
   console.log('request handler \'start\' was called')
-  exec('find /',
-       { timeout: 10000, maxBuffer: 20000 * 1024 },
-       (error, stdout, stderr) => {
-         response.writeHead(200, { 'Content-Type':'text/plain' })
-         response.write(stdout)
-         response.end()
-       })
+  const body = '<html>' +
+          '<head>' +
+          '<meta http-equiv="Content-Type" content="text/html; ' +
+          'charset=UTF-8" />' +
+          '</head>' +
+          '<body>' +
+          '<form action="/upload" method="post">' +
+          '<textarea name="text" rows="20" cols="60"></textarea>' +
+          '<input type="submit" value="Submit text" />' +
+          '</form>' +
+          '</body>' +
+          '</html>'
+
+  response.writeHead(200, { 'Content-Type':'text/html' })
+  response.write(body)
+  response.end()
 }
 
 const upload = (response) => {
